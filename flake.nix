1af17468727012e26496e2a6907dfaf22f0a7093
Kevin Kern <hello@regenrek.at>
2026-01-12 00:38:18 +0100

    refactor(nix): consume nix-clawdbot flake outputs

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-clawdbot.url = "github:clawdbot/nix-clawdbot";
+    nix-clawdbot.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, disko, sops-nix, nix-clawdbot, ... }:
